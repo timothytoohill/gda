303d4cd662f0776273050e626f22a2f48e8a89b4
Timothy Toohill <timothytoohill@invictusinnovations.io>
2022-09-26 12:07:32 -0400

    Fixed another minor issue.

--- ui/src/app/pages/system/logs/mqtt/both.component.ts
+++ ui/src/app/pages/system/logs/mqtt/both.component.ts
@@ -1,15 +1,20 @@
 import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { timer } from 'rxjs';
 import { MQTTLogsComponent } from '../../../../@core/components/mqtt/logs/mqtt.logs.component';
+import { AppStateService } from '../../../../@core/services/app.state.service';
 
 @Component({ selector: 'system-logs-mqtt-both', templateUrl: 'both.component.html', styleUrls: ['both.component.scss'] })
 export class SystemLogsMQTTBothComponent implements AfterViewInit {
     private dashboardTopics = [];
     @ViewChild('mqLogs', { static: false }) private mqLogs: MQTTLogsComponent;
 
+    constructor(public appStateService:AppStateService) {
+        
+    }
+
     ngAfterViewInit() {
         timer(1000).subscribe(() => {
-            this.dashboardTopics = ['inv/#'];
+            this.dashboardTopics = [this.appStateService.appConfigs['appOrganization'] + "/#"];
             this.mqLogs.subscribeToTopics(this.dashboardTopics);
         });
     }
